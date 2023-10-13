df0c01bb6a60170f3f5793c4a7c4106b28c2ce73
Santiagoa03 <santiago.agudelo@oljsolutions.com>
2023-10-13 16:42:58 -0500

    Autenticación login realizada

--- turisTrazo/src/app/auth/pages/login-page/login-page.component.ts
+++ turisTrazo/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Credenciales } from 'src/app/interface/models-type';
 
 @Component({
   selector: 'app-login-page',
@@ -7,8 +10,29 @@ import { Component } from '@angular/core';
 })
 export class LoginPageComponent {
 
+
+  private credenciales!: Credenciales;
+
+  formLogin = this.fb.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+
+  constructor(private authService: AuthService, private fb: FormBuilder) { }
+
   prueba(): void {
-    console.log('Hola');
+
+    if (this.formLogin.invalid) return;
+
+    this.credenciales = {
+      email: this.formLogin.get('email')?.value || '',
+      password: this.formLogin.get('password')?.value || ''
+    }
+    console.log(this.credenciales)
+
+    this.authService.login(this.credenciales);
+
   }
 
 }
