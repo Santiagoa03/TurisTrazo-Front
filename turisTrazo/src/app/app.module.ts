408ef75945048269af1a5749762f305991675f05
Santiagoa03 <santiago.agudelo@oljsolutions.com>
2023-10-19 09:59:59 -0500

    Dependencias

--- turisTrazo/src/app/app.module.ts
+++ turisTrazo/src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared.module';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } 
 import { HttpInterceptor } from './shared/interceptors/http.interceptor';
 
 @NgModule({
